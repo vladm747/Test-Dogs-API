a1b6da4f799cb1a58ffcbbb4eeafd4a3f3e0a9f6
vladm747 <vladyslav.moroz105@gmail.com>
2023-06-01 23:14:04 +0300

    add dbContextFactory, add validation data annotations for model 'dog', add InitialCreate migration

---
+++ DAL/Context/DogContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DAL.Context
+{
+    public class DogContextFactory : IDesignTimeDbContextFactory<DogContext>
+    {
+        public DogContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DogContext>();
+            optionsBuilder.UseSqlServer("Server=.;Database=DogsDB;Trusted_Connection=true;TrustServerCertificate=true;");
+            return new DogContext(optionsBuilder.Options);
+        }
+    }
+}
--- DAL/Models/Dog.cs
+++ DAL/Models/Dog.cs
@@ -5,9 +5,17 @@ namespace DAL.Models
     public class Dog
     {
         [Key]
+        [Required]
+        [StringLength(200)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Color { get; set; }
+        [Required]
+        [RegularExpression(@"/^\d+$/", ErrorMessage = "Tail height is a negative number or is not a number")]
         public long TailLength{ get; set; }
+        [Required]
+        [RegularExpression(@"/^\d+$/", ErrorMessage = "Weight is a negative number or is not a number")]
         public long Weight { get; set; }
     }
 }
